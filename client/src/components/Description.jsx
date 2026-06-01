721a4f21989c20f6c0d7b066c26ec1f7b0f857cf
unknown <alijafry005@gmail.com>
2026-06-02 00:01:33 +0500

    Second commit- Complete Home page and Footer update

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {assets} from '../assets/assets'
+const Description = () => {
+  return (
+    <div className='flex flex-col items-center justify-center my-24 p-6 md:px-28'>
+      <h1 className='text-2x1 sm:text-4x1 font-semibold mb-2 text-xl '>Create AI Images</h1>
+      <p className='text-gray-500 mb-8'>
+        Transform your ideas into stunning visuals with our cutting-edge AI technology.
+      </p>
+
+      <div className='flex flex-col gap-10 md:gap-14 md:flex-row items-center'>
+        <img src={assets.sample_img_1} alt="description" className='w-80 xl:w-96rounded-lg shadow-md' />
+        <div>
+            <h2 className='text-2xl font-medium mt-6'>Introducing AI powered Image Generator</h2>
+            <p className='text-gray-500 mt-4 justify'>Experience the future of creativity with our AI-powered image generator. Transform your ideas into stunning visuals effortlessly. Whether you're a designer, marketer, or content creator, our tool empowers you to bring your vision to life in seconds. Say goodbye to creative blocks and hello to endless possibilities with our cutting-edge technology.</p>
+            <p className='text-gray-500 mt-4'>Simply type in a text prompt, and our cutting-edge AI will generate high-quality images in seconds. From product visuals to characters designs and portraits, even concepts that don't yet exist can be visualized effortlessly.</p>
+
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Description
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -9,6 +9,20 @@ const Header = () => {
       </div>
       <h1 className='text-4xl max-w-[300px] sm:text-7xl sm:max-w-[590px] mx-auto mt-10 text-center'>Turn Text to <span className='text-blue-500'>images</span>, in seconds</h1>
         <p className='text-gray-500 mt-6 text-sm sm:text-base'>Create stunning images with our AI-powered text-to-image generator. Transform your ideas into visuals effortlessly.</p>
+        <button className='sm:test-lg text-white bg-black w-auto mt-8 px-12 py-2.5 flex items-center gap-2 rounded-full'>
+            Generate Images
+            <img className='h-6' src={assets.star_group} alt="Star group" />
+        </button>
+
+        <div className='flex flex-wrap justify-center gap-3 mt-16'>
+            {Array(6).fill('').map((item,index) => (
+                <img key={index} className='rounded-hover:scale-105 transition-all duration-300 cursor-pointer max-sm:w-10' 
+                 src={index%2 ===0 ? assets.sample_img_1 : assets.sample_img_2}
+                 alt="star" width={70} />
+            ))}
+        </div>
+
+        <p className='text-neutral-600 mt-2'>Generated images form Imagify</p>
     </div>
   )
 }
